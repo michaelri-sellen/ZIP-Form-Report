3600d6a14e110ae25ecb9f754fc1afd3bd6b2e52
Michael Rice <MichaelRi@sellen.com>
2020-10-21 20:54:58 -0700

    Update URL and Header

--- generate.js
+++ generate.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
     $.ajax({
-        url: "https://app.asana.com/-/oauth_authorize?response_type=code&client_id=1198884171549959&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob"
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': 'Bearer ' + getUrlVars()["code"]
+        },
+        url: "https://app.asana.com/api/1.0/projects/1146674956832113/tasks"
     }).then(function(data) {
         $('#content').append(data);
     });
 });
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
